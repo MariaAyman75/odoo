6590786d090a71cd5efdec51e8a901ee59c8ee86
MariaAyman75 <mariaayman75@gmail.com>
2024-09-17 17:50:41 +0300

    to do app lab 2

--- todo_app/models/ticket.py
+++ todo_app/models/ticket.py
@@ -11,3 +11,15 @@ class Ticket(models.Model):
     file = fields.Binary()
     assign_to = fields.Many2one('res.users')
     description = fields.Text()
+
+    def action_new(self):
+        print("inside action_new")
+        self.state = 'new'
+
+    def action_doing(self):
+        print("inside action_doing")
+        self.state = 'doing'
+
+    def action_done(self):
+        print("inside action_done")
+        self.state = 'done'
